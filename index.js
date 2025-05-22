ac90acceb4bca792034ae35b8762acbd79a1ad5e
Sayed Sheikh <sayedsheikh9@gmail.com>
2025-05-23 00:36:24 +0600

    fet get and post api added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hktsutd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +24,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    const roommateCollection = client
+      .db("roomDB")
+      .collection("roommateCollection");
+
+    app.get("/flatify", async (req, res) => {
+      const result = await roommateCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/flatify", async (req, res) => {
+      const result = await roommateCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    app.get("/flatify/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await roommateCollection.findOne(query);
+      res.send(result);
+    });
+    app.get("/flatify/mylisting/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = {
+        email: email,
+      };
+      const result = await roommateCollection.find(query).toArray();
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
